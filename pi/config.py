88037cfc91b3f054380276dc25086dd50d0d7115
SeoYeongBaek <happyjessica1004@gmail.com>
2025-11-26 18:00:10 +0900

    아두이노 포트 바뀜

--- pi/config.py
+++ pi/config.py
@@ -1,4 +1,4 @@
-SERIAL_PORT = "/dev/tty.usbmodemHIDPC1"     
+SERIAL_PORT = "/dev/ttyACM0"    
 SERIAL_BAUD =  9600              
 API_BASE    = "https://infinite-stairs-backend.onrender.com"  
 WINDOW_SEC  = 10.0             
